c5350f144c85ece61b410089df28ccbddd464abe
Nurken <xteclu@gmail.com>
2024-10-04 15:56:17 +0500

    init

--- Package.swift
+++ Package.swift
@@ -5,19 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "QboxCallSDKPackage",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "QboxCallSDKPackage",
-            targets: ["QboxCallSDKPackage"]),
+            name: "QboxCallSDK",
+            targets: ["QboxCallSDK"]
+        ),
+    ],
+    dependencies: [
+      .package(url: "https://github.com/stasel/WebRTC.git", from: "126.0.0"),
+      .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "QboxCallSDKPackage"),
-        .testTarget(
-            name: "QboxCallSDKPackageTests",
-            dependencies: ["QboxCallSDKPackage"]),
+          name: "QboxCallSDK",
+          dependencies: [
+            .product(name: "WebRTC", package: "WebRTC"),
+            .product(name: "Starscream", package: "Starscream"),
+          ]
+        )
     ]
 )
